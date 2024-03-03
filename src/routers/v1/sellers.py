5ae88c7fe4a9dae187b0c336b55ed554f6c69864
Semen Khomets <63056333+semen200251@users.noreply.github.com>
2024-03-03 14:50:44 +0300

    First Iteration

---
+++ src/routers/v1/sellers.py
@@ -0,0 +1,73 @@
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+from src.schemas import IncomingSeller, ReturnedSeller, ReturnedAllSellers, ReturnedSellerWithBook, IncomingUpdateSeller
+from fastapi import status, Response, APIRouter, Depends
+from icecream import ic
+from src.configurations.database import get_async_session
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.models.sellers import Seller
+
+sellers_router = APIRouter(
+    tags=["selers"],
+    prefix="/seller"
+)
+
+DBSession = Annotated[AsyncSession, Depends(get_async_session)]
+
+@sellers_router.post("/", response_model=ReturnedSeller, status_code=status.HTTP_201_CREATED)
+async def create_book(seller: IncomingSeller, session: DBSession):
+    new_seller = Seller(first_name = seller.first_name, last_name = seller.last_name, email = seller.email, password = seller.password)
+    session.add(new_seller)
+    await session.flush()
+    return new_seller
+
+@sellers_router.get("/", response_model=ReturnedAllSellers)
+async def get_all_books(session: DBSession):
+    query = select(Seller)
+    res = await session.execute(query)
+    sellers = res.scalars().all()
+    return {"sellers": sellers}
+
+@sellers_router.get("/{seller_id}", response_model=ReturnedSellerWithBook)
+async def get_book(seller_id: int, session: DBSession):
+    seller = await session.execute(
+        select(Seller).options(selectinload(Seller.books)).filter(Seller.id == seller_id)
+    )
+    
+    seller = seller.scalar_one_or_none()
+    return seller
+
+@sellers_router.delete("/{seller_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(seller_id: int, session: DBSession):
+    # Загружаем продавца с предварительной загрузкой связанных книг
+    deleted_seller = await session.execute(
+        select(Seller).options(selectinload(Seller.books)).filter(Seller.id == seller_id)
+    )
+
+    deleted_seller = deleted_seller.scalar_one_or_none()
+
+    if deleted_seller:
+        # Удаляем книги, связанные с продавцом
+        for book in deleted_seller.books:
+            await session.delete(book)
+
+        # Теперь удаляем самого продавца
+        await session.delete(deleted_seller)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@sellers_router.put("/{seller_id}")
+async def update_book(seller_id: int, new_data: IncomingUpdateSeller, session: DBSession):
+    # Оператор "морж", позволяющий одновременно и присвоить значение и проверить его.
+    if updated_seller := await session.get(Seller, seller_id):
+        updated_seller.first_name = new_data.first_name
+        updated_seller.last_name = new_data.last_name
+        updated_seller.email = new_data.email
+
+        await session.flush()
+
+        return updated_seller
+
+    return Response(status_code=status.HTTP_404_NOT_FOUND)
---
+++ src/tests/test_sellers_with_pt.py
@@ -0,0 +1,144 @@
+import pytest
+from fastapi import status
+from sqlalchemy import select
+from json import loads
+from src.models import books, sellers
+
+# Тест на ручку создающую продавца. Тест может ложиться, если другие тесты были запущены вместе с ним (даже для теста books), 
+# так как id не будет 1 (Не знаю, как исправить, поэтому не проверяю, что id=1)
+@pytest.mark.asyncio
+async def test_create_seller(async_client):
+    data = {"first_name": "Wrong Code", "last_name": "Robert Martin", "email": "bad@mail.ru", "password": "12345"}
+    response = await async_client.post("/api/v1/seller/", json=data)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    result_data = response.json()
+
+    assert result_data == {
+        "id": result_data["id"],
+        "first_name": "Wrong Code",
+        "last_name": "Robert Martin", 
+        "email": "bad@mail.ru",
+    }
+
+#тест на ручку получения списка продавцов
+@pytest.mark.asyncio
+async def test_get_sellers(db_session, async_client):
+
+    seller = sellers.Seller(first_name="Pushkin", last_name="Eugeny Onegin", email="bad@mail.ru", password="12345")
+
+    seller2 = sellers.Seller(first_name="Pushkin2", last_name="Eugeny Onegin2", email="bad2@mail.ru", password="123456")
+
+    db_session.add_all([seller, seller2])
+    await db_session.flush()
+
+    response = await async_client.get("/api/v1/seller/")
+
+    assert response.status_code == status.HTTP_200_OK
+
+    assert len(response.json()["sellers"]) == 2  # Опасный паттерн! Если в БД есть данные, то тест упадет
+
+    # Проверяем интерфейс ответа, на который у нас есть контракт.
+    assert response.json() == {
+        "sellers": [
+            {"first_name": "Pushkin", "last_name": "Eugeny Onegin", "email": "bad@mail.ru", "id": seller.id},
+            {"first_name": "Pushkin2", "last_name": "Eugeny Onegin2", "email": "bad2@mail.ru", "id": seller2.id},
+        ]
+    }
+
+# Тест на ручку получения одного продавца
+@pytest.mark.asyncio
+async def test_get_single_seller(db_session, async_client):
+
+    seller = sellers.Seller(first_name="Pushkin", last_name="Eugeny Onegin", email="bad@mail.ru", password="12345")
+
+    seller2 = sellers.Seller(first_name="Pushkin2", last_name="Eugeny Onegin2", email="bad2@mail.ru", password="123456")
+
+    db_session.add_all([seller, seller2])
+    await db_session.flush()
+
+    book = books.Book(author="Pushkin", title="Eugeny Onegin", year=2001, count_pages=104, seller_fk=seller.id)
+    book_2 = books.Book(author="Lermontov", title="Mziri", year=1997, count_pages=104, seller_fk=seller.id)
+    book_3 = books.Book(author="Lermontov2", title="Mziri2", year=1997, count_pages=104, seller_fk=seller2.id)
+    db_session.add_all([book, book_2, book_3])
+    await db_session.flush()
+    books_list = []
+    books_list.append(book)
+    books_list.append(book_2)
+    response = await async_client.get(f"/api/v1/seller/{seller.id}")
+
+    assert response.status_code == status.HTTP_200_OK
+
+    # Проверяем интерфейс ответа, на который у нас есть контракт.
+    response_data = loads(response.text)
+
+    expected_data = {
+        "first_name": "Pushkin",
+        "last_name": "Eugeny Onegin",
+        "email": "bad@mail.ru",
+        "id": seller.id,
+        "books": [
+            dict(id=book.id, title=book.title, author=book.author, year=book.year, count_pages=book.count_pages)
+            for book in books_list
+        ]
+    }
+
+    assert response_data == expected_data
+
+@pytest.mark.asyncio
+async def test_delete_seller(db_session, async_client):
+
+    seller = sellers.Seller(first_name="Pushkin", last_name="Eugeny Onegin", email="bad@mail.ru", password="12345")
+    db_session.add_all([seller])
+    await db_session.flush()
+
+    book = books.Book(author="Pushkin", title="Eugeny Onegin", year=2001, count_pages=104, seller_fk=seller.id)
+    book_2 = books.Book(author="Pushkin2", title="Eugeny Onegin2", year=2001, count_pages=104, seller_fk=seller.id)
+    db_session.add_all([book, book_2])
+    await db_session.flush()
+
+    response = await async_client.delete(f"/api/v1/seller/{seller.id}")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    await db_session.flush()
+
+    all_books = await db_session.execute(select(books.Book))
+    res1 = all_books.scalars().all()
+    assert len(res1) == 0
+    all_sellers = await db_session.execute(select(sellers.Seller))
+    res2 = all_sellers.scalars().all()
+    assert len(res2) == 0
+
+# Тест на ручку обновления продавца
+@pytest.mark.asyncio
+async def test_update_seller(db_session, async_client):
+
+    seller = sellers.Seller(first_name="Pushkin", last_name="Eugeny Onegin", email="bad@mail.ru", password="12345")
+    db_session.add(seller)
+    await db_session.flush()
+
+
+    book = books.Book(author="Pushkin", title="Eugeny Onegin", year=2001, count_pages=104, seller_fk=seller.id)
+
+    db_session.add(book)
+    await db_session.flush()
+
+    response = await async_client.put(
+        f"/api/v1/seller/{seller.id}",
+        json={ "first_name": "Clean Code",
+                "last_name": "Robert Martin",
+                "email": "norm@mail.ru",
+                "id": 3,
+                "password": "5125"},
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    await db_session.flush()
+
+    # Проверяем, что обновились все поля
+    res = await db_session.get(sellers.Seller, seller.id)
+    assert res.first_name == "Clean Code"
+    assert res.last_name == "Robert Martin"
+    assert res.email == "norm@mail.ru"
+    assert res.id == seller.id
